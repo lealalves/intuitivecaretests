6a5156804cf30000b865d4fc2608008f4af9f65d
Vinicius Leal Alves <leealalves@hotmail.com>
2023-05-26 01:09:20 -0300

    added skip and limit in search

--- teste4/main.py
+++ teste4/main.py
@@ -1,63 +1,53 @@
 import re
 import pandas as pd
-from flask import Flask, jsonify, request, json
+from flask import Flask, request
 
 df = pd.read_csv('Relatorio_cadop.csv', encoding='iso-8859-1', on_bad_lines='skip', sep=';', skiprows=1, dtype=str, keep_default_na=False)
 
-# app = Flask(__name__)
-# json.provider.DefaultJSONProvider.ensure_ascii = False
+app = Flask(__name__)
 
 dfr = pd.DataFrame()
 
 dfc = list(df.columns)
 
-result_list = []
 
-for i in range(len(dfc)):
-    filter = df[dfc[i]].str.contains('registro', flags=re.UNICODE, case=False)
-    result = df[filter]
-
-    print(f'{len(result)} resultados na coluna: {dfc[i]}')
-    print(f'RESULTADO: {result}')
-    
-    if len(result) > 0:
-        result_list.append(result)
+@app.route('/')
+def hello_word():
+    return 'Hello Word!'
 
 
-dfr = pd.concat(result_list).drop_duplicates()
-print(f'RESULTADO FINAL: {dfr}, TAMANHO {len(dfr)}')
+@app.get('/cadastros')
+def cadastros():
+    args = request.args
 
-# @app.route('/')
-# def hello_word():
-#     return 'Hello Word!'
-#
-#
-# @app.get('/cadastros')
-# def cadastros():
-#     args = request.args
-#     busca = args.get('busca')
-#
-#     if busca:
-#         dfr = pd.DataFrame()
-#
-#         dfc = list(df.columns)
-#
-#         for i in range(len(dfc)):
-#             filter = df[dfc[i]].str.contains(busca, flags=re.UNICODE, case=False)
-#
-#             print(f'{len(df[filter])} resultados na coluna: {dfc[i]}')
-#
-#             if len(df[filter]) > 0:
-#                 dfr = pd.concat([df[filter]])
-#
-#         data = dfr.to_json(force_ascii=False, orient='records')
-#         return jsonify(data)
-#     else:
-#         data = df.to_json(force_ascii=False, orient='records')
-#         return jsonify(data)
-#
-#
-# app.run(port=3000)
+    busca = args.get('busca')
+    skip = 0 if args.get('skip') is None else int(args.get('skip'))
+    limit = 10 if args.get('limit') is None else int(args.get('limit'))
+
+    if busca:
+        result_list = []
+        for i in range(len(dfc)):
+            filter = df[dfc[i]].str.contains(busca, flags=re.UNICODE, case=False)
+            result = df[filter]
+
+            if len(result) > 0:
+                result_list.append(result)
+
+        if len(result_list) == 0:
+            return {
+                'message': 'No results found'
+            }
+
+        dfr = pd.concat(result_list, ignore_index=True).drop_duplicates()
+        data = dfr.loc[skip:limit]
+
+        return data.to_json(force_ascii=False)
+    else:
+        data = df.loc[skip:limit]
+        return data.to_json(force_ascii=False)
+
+
+app.run(port=3000, debug=True)
 
 
 
