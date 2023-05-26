337e7260576626ff8accad7286ce5f765fadcd2b
Vinicius Leal Alves <leealalves@hotmail.com>
2023-05-26 20:23:01 -0300

    removed skip and limit queries

--- teste4/main.py
+++ teste4/main.py
@@ -1,10 +1,11 @@
 import re
 import pandas as pd
 from flask import Flask, request
-
+from flask_cors import CORS
 df = pd.read_csv('Relatorio_cadop.csv', encoding='iso-8859-1', on_bad_lines='skip', sep=';', skiprows=1, dtype=str, keep_default_na=False)
 
 app = Flask(__name__)
+CORS(app)
 
 dfr = pd.DataFrame()
 
@@ -21,30 +22,26 @@ def cadastros():
     args = request.args
 
     busca = args.get('busca')
-    skip = 0 if args.get('skip') is None else int(args.get('skip'))
-    limit = 10 if args.get('limit') is None else int(args.get('limit'))
 
     if busca:
         result_list = []
         for i in range(len(dfc)):
             filter = df[dfc[i]].str.contains(busca, flags=re.UNICODE, case=False)
             result = df[filter]
-
+            print(busca)
+            print(f'{len(result)} resultados na coluna {dfc[i]}')
             if len(result) > 0:
                 result_list.append(result)
 
         if len(result_list) == 0:
-            return {
-                'message': 'No results found'
-            }
+            print('sem resultados')
+            return result_list
 
         dfr = pd.concat(result_list, ignore_index=True).drop_duplicates()
-        data = dfr.loc[skip:limit]
 
-        return data.to_json(force_ascii=False)
+        return dfr.to_json(force_ascii=False, orient='values')
     else:
-        data = df.loc[skip:limit]
-        return data.to_json(force_ascii=False)
+        return df.to_json(force_ascii=False, orient='values')
 
 
 app.run(port=3000, debug=True)
